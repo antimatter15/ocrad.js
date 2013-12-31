3c21773a4a7c5ccfdb53d9c1c7bca13803c11077
antimatter15 <antimatter15@gmail.com>
2013-12-30 23:23:33 -0500

    writing some words

--- ocrad.js
+++ ocrad.js
@@ -7999,25 +7999,6 @@ if (Module['noInitialRun']) {
 run();
 // {{POST_RUN_ADDITIONS}}
 // {{MODULE_ADDITIONS}}
-var api = { // low level api
-	'version': Module.cwrap('OCRAD_version', 'string'),
-	'open': Module.cwrap('OCRAD_open', 'number'),
-	'close': Module.cwrap('OCRAD_close', 'number', ['number']),
-	'get_errno': Module.cwrap('OCRAD_get_errno', 'number', ['number']),
-	'set_image': Module.cwrap('OCRAD_set_image', 'number', ['number', 'number', 'number']),
-	'set_image_from_file': Module.cwrap('OCRAD_set_image_from_file', 'number', ['number', 'string', 'number']),
-	'set_utf8_format': Module.cwrap('OCRAD_set_utf8_format', 'number', ['number', 'number']),
-	'set_threshold': Module.cwrap('OCRAD_set_threshold', 'number', ['number', 'number']),
-	'scale': Module.cwrap('OCRAD_scale', 'number', ['number', 'number']),
-	'recognize': Module.cwrap('OCRAD_recognize', 'number', ['number', 'number']),
-	'result_blocks': Module.cwrap('OCRAD_result_blocks', 'number', ['number']),
-	'result_lines': Module.cwrap('OCRAD_result_lines', 'number', ['number', 'number']),
-	'result_chars_total': Module.cwrap('OCRAD_result_chars_total', 'number', ['number']),
-	'result_chars_block': Module.cwrap('OCRAD_result_chars_block', 'number', ['number', 'number']),
-	'result_chars_line': Module.cwrap('OCRAD_result_chars_line', 'number', ['number', 'number', 'number']),
-	'result_line': Module.cwrap('OCRAD_result_line', 'string', ['number', 'number', 'number']),
-	'result_first_character': Module.cwrap('OCRAD_result_first_character', 'number', ['number'])
-}
 var OCRAD = function(image){
 	if(image.getContext) image = image.getContext('2d');
 	if(image.getImageData) image = image.getImageData(0, 0, image.canvas.width, image.canvas.height);
@@ -8041,26 +8022,43 @@ var OCRAD = function(image){
 	for (; i < srcLength; i += 4, ++j) {
 		dst[j] = (src[i] * coeff_r + src[i+1] * coeff_g + src[i+2] * coeff_b + 8192) >> 14;
 	}
-	FS.writeFile('/in.pnm', dst, {encoding: 'binary'});
-	var desc = api.open();
-	api.set_image_from_file(desc, 'in.pnm', 0);
-	
-	api.set_utf8_format(desc, 1);
-	api.recognize(desc, 0)
+	OCRAD.write_file('/in.pnm', dst);
+	var desc = OCRAD.open();
+	OCRAD.set_image_from_file(desc, 'in.pnm', 0);
+	OCRAD.set_utf8_format(desc, 1);
+	OCRAD.recognize(desc, 0)
 	var text = '';
-	var block_count = api.result_blocks(desc);
+	var block_count = OCRAD.result_blocks(desc);
 	
 	for(var i = 0; i < block_count; i++){
-		var line_count = api.result_lines(desc, i);
+		var line_count = OCRAD.result_lines(desc, i);
 		for(var j = 0; j < line_count; j++){
-			var line = api.result_line(desc, i, j);
-			// lines.push(line)
+			var line = OCRAD.result_line(desc, i, j);
 			text += line;
 		}
 	}
-	api.close(desc)
+	OCRAD.close(desc)
 	return text;
 }
-OCRAD.version = api.version;
+OCRAD.write_file = function(filename, arr){
+	FS.writeFile(filename, arr, {encoding: 'binary'});
+}
+OCRAD.version = Module.cwrap('OCRAD_version', 'string');
+OCRAD.open = Module.cwrap('OCRAD_open', 'number');
+OCRAD.close = Module.cwrap('OCRAD_close', 'number', ['number']);
+OCRAD.get_errno = Module.cwrap('OCRAD_get_errno', 'number', ['number']);
+OCRAD.set_image = Module.cwrap('OCRAD_set_image', 'number', ['number', 'number', 'number']);
+OCRAD.set_image_from_file = Module.cwrap('OCRAD_set_image_from_file', 'number', ['number', 'string', 'number']);
+OCRAD.set_utf8_format = Module.cwrap('OCRAD_set_utf8_format', 'number', ['number', 'number']);
+OCRAD.set_threshold = Module.cwrap('OCRAD_set_threshold', 'number', ['number', 'number']);
+OCRAD.scale = Module.cwrap('OCRAD_scale', 'number', ['number', 'number']);
+OCRAD.recognize = Module.cwrap('OCRAD_recognize', 'number', ['number', 'number']);
+OCRAD.result_blocks = Module.cwrap('OCRAD_result_blocks', 'number', ['number']);
+OCRAD.result_lines = Module.cwrap('OCRAD_result_lines', 'number', ['number', 'number']);
+OCRAD.result_chars_total = Module.cwrap('OCRAD_result_chars_total', 'number', ['number']);
+OCRAD.result_chars_block = Module.cwrap('OCRAD_result_chars_block', 'number', ['number', 'number']);
+OCRAD.result_chars_line = Module.cwrap('OCRAD_result_chars_line', 'number', ['number', 'number', 'number']);
+OCRAD.result_line = Module.cwrap('OCRAD_result_line', 'string', ['number', 'number', 'number']);
+OCRAD.result_first_character = Module.cwrap('OCRAD_result_first_character', 'number', ['number']);
 return OCRAD;
 })();
